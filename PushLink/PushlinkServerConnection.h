77a0062cadc0c8cca0acfa599977c4b675fd8e13
David Stalnaker <david.stalnaker@gmail.com>
2011-11-25 10:55:49 -0500

    created class to encapsulate server calls

---
+++ PushLink/PushlinkServerConnection.h
@@ -0,0 +1,15 @@
+//
+//  PushlinkServerConnection.h
+//  PushLink
+//
+//  Created by David Stalnaker on 11/24/11.
+//  Copyright (c) 2011 Rochester Institute of Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PushlinkServerConnection : NSObject
+
+@property (strong, nonatomic) NSString *passcode;
+
+@end
---
+++ PushLink/PushlinkServerConnection.m
@@ -0,0 +1,15 @@
+//
+//  PushlinkServerConnection.m
+//  PushLink
+//
+//  Created by David Stalnaker on 11/24/11.
+//  Copyright (c) 2011 Rochester Institute of Technology. All rights reserved.
+//
+
+#import "PushlinkServerConnection.h"
+
+@implementation PushlinkServerConnection
+
+@synthesize passcode;
+
+@end
